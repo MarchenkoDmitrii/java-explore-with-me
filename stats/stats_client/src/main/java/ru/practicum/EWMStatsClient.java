0df018a553f1922265ad754cba7f3d4b76efc46a
DmitriiMarchenko <dmitri.marchenko96@yandex.ru>
2024-04-03 23:58:27 +0500

    first

---
+++ stats/stats_client/src/main/java/ru/practicum/EWMStatsClient.java
@@ -0,0 +1,11 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWMStatsClient {
+    public static void main(String[] args) {
+        SpringApplication.run(EWMStatsClient.class, args);
+    }
+}
